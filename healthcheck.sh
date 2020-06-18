a37a5a7f65227e63335f6e02ab53a3e0c19f2ec0
Gilberto Muñoz <gilberto@generalsoftwareinc.com>
2020-06-18 10:11:30 -0400

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,7 +2,7 @@
 
 
 if [[ "$HADOOP_SERVICES" == *"namenode"* ]]; then
-    if [[ -z $HEALTHCHECK_URL_NAMENODE ]]
+    if [[ -z $HEALTHCHECK_URL_NAMENODE ]]; then
         export HEALTHCHECK_URL_NAMENODE="localhost:9870"
     fi
     if [[ $(curl -LI "$HEALTHCHECK_URL_NAMENODE/jmx" -s -o /dev/null -w "%{http_code}") -ne 200 ]]; then
@@ -10,7 +10,7 @@ if [[ "$HADOOP_SERVICES" == *"namenode"* ]]; then
     fi
 fi
 if [[ "$HADOOP_SERVICES" == *"resourcemanager"* ]]; then
-    if [[ -z $HEALTHCHECK_URL_RESOURCEMANAGER ]]
+    if [[ -z $HEALTHCHECK_URL_RESOURCEMANAGER ]]; then
         export HEALTHCHECK_URL_RESOURCEMANAGER="localhost:8088"
     fi
     if [[ $(curl -LI "$HEALTHCHECK_URL_RESOURCEMANAGER/jmx" -s -o /dev/null -w "%{http_code}") -ne 200 ]]; then
@@ -18,7 +18,7 @@ if [[ "$HADOOP_SERVICES" == *"resourcemanager"* ]]; then
     fi
 fi
 if [[ "$HADOOP_SERVICES" == *"historyserver"* ]]; then
-    if [[ -z $HEALTHCHECK_URL_HISTORYSERVER ]]
+    if [[ -z $HEALTHCHECK_URL_HISTORYSERVER ]]; then
         export HEALTHCHECK_URL_HISTORYSERVER="localhost:19888"
     fi
     if [[ $(curl -LI "$HEALTHCHECK_URL_HISTORYSERVER/jmx" -s -o /dev/null -w "%{http_code}") -ne 200 ]]; then
@@ -27,7 +27,7 @@ if [[ "$HADOOP_SERVICES" == *"historyserver"* ]]; then
 fi
 
 if [[ "$HADOOP_SERVICES" == *"nodemanager"* ]]; then
-    if [[ -z $HEALTHCHECK_URL_NODEMANAGER ]]
+    if [[ -z $HEALTHCHECK_URL_NODEMANAGER ]]; then
         export HEALTHCHECK_URL_NODEMANAGER="localhost:8042"
     fi
     if [[ $(curl -LI "$HEALTHCHECK_URL_NODEMANAGER/jmx" -s -o /dev/null -w "%{http_code}") -ne 200 ]]; then
@@ -35,7 +35,7 @@ if [[ "$HADOOP_SERVICES" == *"nodemanager"* ]]; then
     fi
 fi
 if [[ "$HADOOP_SERVICES" == *"datanode"* ]]; then
-    if [[ -z $HEALTHCHECK_URL_DATANODE ]]
+    if [[ -z $HEALTHCHECK_URL_DATANODE ]]; then
         export HEALTHCHECK_URL_DATANODE="localhost:9864"
     fi
     if [[ $(curl -LI "$HEALTHCHECK_URL_DATANODE/jmx" -s -o /dev/null -w "%{http_code}") -ne 200 ]]; then
